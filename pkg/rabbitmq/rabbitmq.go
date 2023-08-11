89118362da391de7d8e55ddb700f92e2a132eccd
ywj <1026579245@qq.com>
2023-08-11 15:32:29 +0800

    feat:修改协程池数量

--- pkg/rabbitmq/rabbitmq.go
+++ pkg/rabbitmq/rabbitmq.go
@@ -24,7 +24,7 @@ import (
 )
 
 const (
-	PoolCapPreCoreNum      int  = 2
+	PoolCapPreCoreNum      int  = 1
 	max_try_times          int8 = 3
 	default_coroutine_nums int  = 10
 	max_coroutine_nums     int  = 30
